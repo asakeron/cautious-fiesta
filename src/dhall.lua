03bb5cd5c981abb193104bcb43d8bb9d78f1d69b
André <abs+github@emancipe.se>
2023-05-14 09:16:23 +0000

    initial support to utf-8

--- src/dhall.lua
+++ src/dhall.lua
@@ -1,15 +1,4 @@
---[[
-    dhall.lua - A pure-lua implementation of a subset of Dhall.
-
-    This grammar currently supports parsing of Dhall comments, which include
-    block comments and line comments.
-
-    Block comments:
-        Block comments start with '{-' and end with '-}'. They can be nested.
-
-    Line comments:
-        Line comments start with '--' and continue to the end of the line.
-]]
+-- dhall.lua - A pure-lua implementation of a subset of Dhall.
 local lpeg = require("lpeg")
 
 local grammar = {
@@ -17,27 +6,62 @@ local grammar = {
     "S",
     S = lpeg.V("block_comment") + lpeg.V("line_comment"),
 
-    -- Character classes
-    -- TODO: Dhall fully supports UTF-8. This implementation currently only supports printable ASCII.
+    -- continuation byte for multi-byte UTF-8 sequences
+    cont = lpeg.R("\128\191"),
+
+    -- UTF-8 decoding patterns
+    utf8 = lpeg.R("\194\223") * lpeg.V("cont")
+         + lpeg.R("\224\239") * lpeg.V("cont") ^ 2
+         + lpeg.R("\240\244") * lpeg.V("cont") ^ 3,
+
     printable_ascii = lpeg.R("\32\127"),
-    tab = lpeg.P("\t"),
+
+    -- NOTE: There are many line endings in the wild
+    --
+    -- See: https://en.wikipedia.org/wiki/Newline
+    --
+    -- For simplicity this supports Unix and Windows line-endings, which are the most
+    -- common
     end_of_line = lpeg.P("\n")
                 + lpeg.P("\r\n"),
 
-    -- Block comments
+    -- This rule matches all characters that are not:
+    --
+    -- * not ASCII
+    -- * not part of a surrogate pair
+    -- * not a "non-character"
+    valid_non_ascii = lpeg.V("utf8"),
+
+    tab = lpeg.P("\t"),
+
     block_comment_char = lpeg.V("printable_ascii")
+                       + lpeg.V("valid_non_ascii")
                        + lpeg.V("tab")
                        + lpeg.V("end_of_line"),
+
     block_comment = lpeg.P("{-") * lpeg.V("block_comment_continue"),
+
     block_comment_continue = lpeg.P("-}")
                            + lpeg.V("block_comment") * lpeg.V("block_comment_continue")
                            + lpeg.V("block_comment_char") * lpeg.V("block_comment_continue"),
 
-    -- Line comments
     not_end_of_line = lpeg.V("printable_ascii")
+                    + lpeg.V("valid_non_ascii")
                     + lpeg.V("tab"),
+
     line_comment_prefix = lpeg.P("--") * (lpeg.V("not_end_of_line") ^ 0),
-    line_comment = lpeg.V("line_comment_prefix") * lpeg.V("end_of_line")
+
+    line_comment = lpeg.V("line_comment_prefix") * lpeg.V("end_of_line"),
+
+    whitespace_chunk = lpeg.P(" ")
+                     + lpeg.V("tab")
+                     + lpeg.V("end_of_line")
+                     + lpeg.V("line_comment")
+                     + lpeg.V("block_comment"),
+
+    whsp = lpeg.V("whitespace_chunk") ^ 0,
+
+    whsp1 = lpeg.V("whitespace_chunk") ^ 1
 }
 
 return lpeg.P(grammar)
--- src/dhall.spec.lua
+++ src/dhall.spec.lua
@@ -23,3 +23,48 @@ describe("Dhall Parser", function()
         assert.are.equal(line_and_block:len() + 1, parser:match(line_and_block))
     end)
 end)
+
+describe("Dhall Parser UTF-8 tests", function()
+    local parser = require("dhall")
+
+    it("parses UTF-8 characters", function()
+        local utf8_in_line_comment = "-- a line comment with UTF-8: åäö\n"
+        local utf8_in_block_comment = "{- a block comment with UTF-8: åäö -}"
+        local utf8_in_nested_block_comment = "{- a nested block comment with UTF-8: åäö {- åäö -} åäö -}"
+
+        assert.are.equal(utf8_in_line_comment:len() + 1, parser:match(utf8_in_line_comment))
+        assert.are.equal(utf8_in_block_comment:len() + 1, parser:match(utf8_in_block_comment))
+        assert.are.equal(utf8_in_nested_block_comment:len() + 1, parser:match(utf8_in_nested_block_comment))
+    end)
+
+    it("parses valid non-ASCII characters", function()
+        local non_ascii_in_line_comment = "-- a line comment with non-ASCII: €\n"
+        local non_ascii_in_block_comment = "{- a block comment with non-ASCII: € -}"
+        local non_ascii_in_nested_block_comment = "{- a nested block comment with non-ASCII: € {- € -} € -}"
+
+        assert.are.equal(non_ascii_in_line_comment:len() + 1, parser:match(non_ascii_in_line_comment))
+        assert.are.equal(non_ascii_in_block_comment:len() + 1, parser:match(non_ascii_in_block_comment))
+        assert.are.equal(non_ascii_in_nested_block_comment:len() + 1, parser:match(non_ascii_in_nested_block_comment))
+    end)
+
+    it("does not parse invalid UTF-8 sequences", function()
+        -- This is an invalid UTF-8 sequence (in hexadecimal): C3 28
+        local invalid_utf8 = "-- a line comment with invalid UTF-8: \195\40\n"
+
+        assert.is_nil(parser:match(invalid_utf8))
+    end)
+
+    it("does not parse surrogate pairs", function()
+        -- This is a surrogate pair (in hexadecimal): D800 DC00
+        local surrogate_pair = "-- a line comment with a surrogate pair: \216\0\220\0\n"
+
+        assert.is_nil(parser:match(surrogate_pair))
+    end)
+
+    it("does not parse non-characters", function()
+        -- This is a non-character (in hexadecimal): FDD0
+        local non_character = "-- a line comment with a non-character: \253\208\n"
+
+        assert.is_nil(parser:match(non_character))
+    end)
+end)
